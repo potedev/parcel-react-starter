0618323775018595683e6035814800fab15d0c83
potedev <quentin@pote.dev>
2021-06-11 10:58:12 +0200

    [Register] Added some error logic along with a loading state

--- src/js/components/inputGroup.jsx
+++ src/js/components/inputGroup.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export const InputGroup = (props) => {
-    const { handleChange, label, type, required, minLength, maxLength } = props;
+    const { handleChange, isValid, label, type, required, minLength, maxLength } = props;
 
     // const handleChange = (event) => {
     //     console.log(`L'input : ${label} de type :${type} est : `, event.target.value);
     // }
 
     return (
-        <div>
+        <div style={!isValid ? { backgroundColor: "red" } : {}}>
             <label>{label}</label>
             <input
                 onChange={(e) => handleChange(e.target.value)}
--- src/js/pages/Register.jsx
+++ src/js/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { InputGroup } from '../components/inputGroup'
 import { SubmitButton } from '../components/submitButton'
+import api from '../../utils/api'
 
 //High Order Component
 export const Register = () => {
@@ -11,36 +12,57 @@ export const Register = () => {
     // }
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+    const [fieldError, setFieldError] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        setError(null)
+        setFieldError(null)
+        setIsLoading(false)
+
         console.log('Register state : Email -> ', email)
         console.log('Register state : Password -> ', password)
 
-        let body = {
-            email,
-            password
+        // let body = {
+        //     email,
+        //     password
+        // }
+
+        // const result = await fetch("http://localhost:4000/api/v1/users/", {
+        //     method: "POST",
+        //     body: JSON.stringify(body),
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     }
+        // })
+
+        // console.log('result with a fetch method', result);
+
+        try {
+            setIsLoading(true)
+            const axiosResult = await api.post('users/', { email, password });
+            console.log('result with an axios method', axiosResult);
+            setIsLoading(false)
         }
-
-        let result = await fetch("http://localhost:4000/api/v1/users/", {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-
-        console.log('result', result);
+        catch (err) {
+            console.log('axios err :', err.response);
+            setError(err.response?.data?.message);
+            setFieldError(err.response?.data?.field);
+            setIsLoading(false)
+        }
     }
 
     return (
         <div>
             <h1>Register</h1>
+            <p style={{ color: "red" }}>{error && error}</p>
+            {isLoading && <p>Loading...</p>}
             <form onSubmit={handleSubmit}>
-                <InputGroup handleChange={setEmail} label="Email" type="email" required />
-                <InputGroup handleChange={setPassword} label="Password" type="password" required={true} minLength="3" maxLength="15" />
-                <InputGroup label="Username" type="text" />
+                <InputGroup handleChange={setEmail} isValid={fieldError !== "email"} label="Email" type="email" required />
+                <InputGroup handleChange={setPassword} isValid={fieldError !== "password"} label="Password" type="password" required={true} minLength="1" maxLength="15" />
                 <SubmitButton name="S'enregistrer" />
             </form>
         </div>
